994bf8a8887988cf9c67c08ef7359f9f714cc9f6
Anticoder03 <116935348+Anticoder03@users.noreply.github.com>
2023-12-24 12:09:12 +0530

    Add files via upload

---
+++ WindowsApplication7/WindowsApplication7/Form1.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Dim r, area As Integer
+        r = InputBox("Enter Radious Of Circle: ")
+        area = 3.14 * r * r
+        MsgBox("Artea of circle is: " & area)
+    End Sub
+End Class
