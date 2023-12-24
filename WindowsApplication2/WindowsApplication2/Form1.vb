5db1d2e8e49ef9562856a47235fe28037e6a39ef
Anticoder03 <116935348+Anticoder03@users.noreply.github.com>
2023-12-24 12:04:40 +0530

    Add files via upload

---
+++ WindowsApplication2/WindowsApplication2/Form1.vb
@@ -0,0 +1,35 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Dim a, b, c As Integer
+        a = TextBox1.Text
+        b = TextBox2.Text
+        c = a + b
+        TextBox3.Text = c
+
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        Dim a, b, c As Integer
+        a = TextBox1.Text
+        b = TextBox2.Text
+        c = a - b
+        TextBox4.Text = c
+    End Sub
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+        Dim a, b, c As Integer
+        a = TextBox1.Text
+        b = TextBox2.Text
+        c = a * b
+        TextBox5.Text = c
+    End Sub
+
+    Private Sub Button4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button4.Click
+        Dim a, b, c As Integer
+        a = TextBox1.Text
+        b = TextBox2.Text
+        c = a / b
+        TextBox6.Text = c
+    End Sub
+End Class
